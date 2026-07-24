35557269c30456f49502f3b3d428fc316508f982
hashimtech <hashimtech336@gmail.com>
2026-07-24 15:38:01 +0530

    fix: use HashRouter for GitHub Pages, fix favicon path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { useEffect } from 'react';
 import { AppRoutes } from './routes/AppRoutes';
 import { useAuthStore } from './store/useAuthStore';
